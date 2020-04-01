1bc3f589045a58d0d38d1599113b622dc65bf676
lsankar4033 <lsankar4033@gmail.com>
2020-04-01 09:57:48 -0700

    write_genesis_state

--- stethoscope/genesis_state.py
+++ stethoscope/genesis_state.py
@@ -10,16 +10,17 @@ prepare_config('./config', 'minimal')
 reload(spec)
 
 
-def build_genesis_state():
+def write_genesis_state():
     # NOTE: taken from pyspec
     eth1_block_hash = b'\x12' * 32
     eth1_timestamp = spec.MIN_GENESIS_TIME
 
-    print(f'Building {spec.MIN_GENESIS_ACTIVE_VALIDATOR_COUNT} deposits')
     deposits, deposit_root, _ = prepare_genesis_deposits(
         spec, spec.MIN_GENESIS_ACTIVE_VALIDATOR_COUNT, spec.MAX_EFFECTIVE_BALANCE, signed=True)
 
-    print('Initializing state')
     state = initialize_beacon_state_from_eth1(eth1_block_hash, eth1_timestamp, deposits)
+
+    with open('ssz/minimal.ssz', 'wb') as w:
+        state.serialize(w)
 
     return state
