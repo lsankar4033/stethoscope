ad2efdfe5dbdcf39b99923db12ccf5fb9749f2a6
lsankar4033 <lsankar4033@gmail.com>
2020-04-06 18:30:34 -0700

    Add rumor trio fixture

--- stethoscope/test_sync_rpc.py
+++ stethoscope/test_sync_rpc.py
@@ -14,7 +14,7 @@ ENV = os.environ.get('STETHOSCOPE_ENV') or 'development'
 
 
 @pytest.fixture(scope='session')
-def genesis_path(request):
+def genesis_path():
     print(f'writing new genesis file at {GENESIS_PATH}')
 
     # Don't rewrite cached file unless in production
@@ -27,26 +27,42 @@ def genesis_path(request):
         os.remove(GENESIS_PATH)
 
 
-# NOTE: this isn't the right abstraction when we consider multiple genesis files and more complicated (i.e.
-# multi-client tests)
+# NOTE: below, 'client' and 'rumor' may be renamed once we include multi-client tests as well as these
+# single-client ones
+
 @pytest.fixture(scope='session', params=['lighthouse'])
-def single_client(request, genesis_path):
+def client(request, genesis_path):
     client = build_client(request.param, genesis_path)
 
     print(f'starting client of type {request.param}\n')
     client.start()
 
-    # TODO: create rumor instance attached to this client!
     yield client
 
     print(f'closing client of type {request.param}\n')
     client.stop()
 
 
-async def test_status(single_client):
+# NOTE: trio fixture
+@pytest.fixture
+async def rumor(client):
+    print('starting rumor instance')
+    async with Rumor(cmd='rumor') as rumor:
+        await rumor.host.start()
+        await rumor.host.listen(tcp=9000)
+        print('set up rumor instance')
+
+        print(f'attempting to connect to client with ENR {client.enr()}')
+        client_id = await rumor.peer.connect(client.enr()).peer_id()
+        print(f'connected rumor to client with peer ID {client_id}')
+
+        yield rumor
+
+
+async def test_status(rumor):
     # async with rumor (decorator)
     # connect rumor <-> client (helper fn?)
     # try status message on rumor
     # check response
-    print(f'Client: {single_client}')
+    print(f'Rumor: {rumor}')
     assert True
