4a3fea544fe4a8e0f419da04c7e51f0b84344bd0
lsankar4033 <lsankar4033@gmail.com>
2020-04-11 18:53:24 -0700

    WIP blocks-by-range v1/v2

--- stethoscope/reqresp.py
+++ stethoscope/reqresp.py
@@ -2,6 +2,8 @@ from eth2spec.utils.ssz.ssz_typing import (
     Bytes4, Bytes32, Container, uint64
 )
 
+from eth2spec.phase0.spec import Root, Slot
+
 
 class Status(Container):
     version: Bytes4
@@ -9,3 +11,16 @@ class Status(Container):
     finalized_epoch: uint64
     head_root: Bytes32
     head_slot: uint64
+
+
+class BlocksByRangeReqV1(Container):
+    head_block_root: Root
+    start_slot: Slot
+    count: uint64
+    step: uint64
+
+
+class BlocksByRangeReqV2(Container):
+    start_slot: Slot
+    count: uint64
+    step: uint64
