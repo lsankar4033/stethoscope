e5164e2a9fe8a7e6fe02ab50220015b350e7b71e
lsankar4033 <lsankar4033@gmail.com>
2020-05-28 16:20:32 -0700

    Allow running tests for a specific client at a time

--- run.py
+++ run.py
@@ -3,19 +3,19 @@ import os
 import yaml
 
 from lib.console import ConsoleWriter
-from lib.fixtures import extract_fixtures, setup_fixture, teardown_fixture
+from lib.fixtures import all_clients, extract_fixtures, setup_fixture, teardown_fixture
 from lib.runner import run_test_suite
 
 TESTS_DIR = 'tests'
 
 
-def run_suite(suite):
+def run_suite(suite, clients):
     suite_file = f'{TESTS_DIR}/{suite}.yml'
     with open(suite_file, 'r') as f:
         full_config = yaml.load(f, Loader=yaml.Loader)
 
     cw = ConsoleWriter(suite, None, None)
-    fixtures = extract_fixtures(full_config)
+    fixtures = extract_fixtures(full_config, clients)
 
     for fixture in fixtures:
         cw = cw._replace(fixture=fixture.name)
@@ -34,20 +34,23 @@ def run_suite(suite):
             teardown_fixture(fixture)
 
 
-def run_all_suites():
+def run_all_suites(clients):
     for suite_file in os.listdir(TESTS_DIR):
         if suite_file.endswith('.yml'):
-            run_suite(suite_file[0:len(suite_file) - 4])
+            run_suite(suite_file[0:len(suite_file) - 4], clients)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run a single stethoscope test')
     parser.add_argument('-t', '--test', help='run a specific test suite by name')
+    parser.add_argument(
+        '-c', '--client', help=f'run test(s) for a specific client. possible values: {all_clients}')
 
     args = parser.parse_args()
+    clients = all_clients if args.client is None else [args.client]
 
     if args.test is None:
-        run_all_suites()
+        run_all_suites(clients)
 
     else:
-        run_suite(args.test)
+        run_suite(args.test, clients)
