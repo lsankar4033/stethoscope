2040633d3dbab0f366109b2802e044891cb9e11e
lsankar4033 <lsankar4033@gmail.com>
2020-07-14 13:31:17 -0700

    Enclose dv5 test in try/finally

--- scripts/dv5/single_node.py
+++ scripts/dv5/single_node.py
@@ -5,22 +5,24 @@ import trio
 async def run(args):
     async with SubprocessConn(cmd='rumor bare') as conn:
         async with trio.open_nursery() as nursery:
-            rumor = Rumor(conn, nursery)
-            await rumor.host.start()
+            try:
+                rumor = Rumor(conn, nursery)
+                await rumor.host.start()
 
-            # TODO: check type of result
-            enr = await rumor.enr.make()
+                # TODO: check type of result
+                enr = await rumor.enr.make()
 
-            await rumor.dv5.run(args['enr'])
+                await rumor.dv5.run(args['enr'])
 
-            # TODO: result validation
-            result = await rumor.dv5.ping(target=args['enr'])
+                # TODO: result validation
+                result = await rumor.dv5.ping(target=args['enr'])
 
-            # TODO: result validation
-            result = await rumor.dv5.lookup(target=args['enr')
+                # TODO: result validation
+                result = await rumor.dv5.lookup(target=args['enr')
 
-            await rumor.dv5.cancel()
+                await rumor.dv5.cancel()
 
-            nursery.cancel_scope.cancel()
+                return (0, None)
 
-            return (0, None)
+            finally:
+                nursery.cancel_scope.cancel()
